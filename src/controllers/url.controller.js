ea8017834e981942cedaa1e8f5f97208b035cbf9
antonig5 <giraldoantonio160@gmail.com>
2024-01-30 18:12:23 -0500

    Adding new functionalities

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -43,3 +43,42 @@ export const CreatedShotLink = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+export const DeleteAll = async (req, res) => {
+  try {
+    const urls = await Url.deleteMany();
+    res.json(urls);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const VerifyUrl = async (req, res) => {
+  const apiKey = "AIzaSyDScdBM8h_qfkSqxsNU2fWhSRbjWYDgpe4";
+  const safeBrowsingURL = `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${apiKey}`;
+
+  try {
+    const { url } = req.body;
+    const response = await fetch(safeBrowsingURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client: {
+          clientId: "api-link",
+          clientVersion: "1.5.2",
+        },
+        threatInfo: {
+          threatTypes: ["MALWARE", "SOCIAL_ENGINEERING"],
+          platformTypes: ["WINDOWS"],
+          threatEntryTypes: ["URL"],
+          threatEntries: [{ url: url }],
+        },
+      }),
+    });
+    const data = await response.json();
+    res.json({ data: data });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routers/url.router.js
+++ src/routers/url.router.js
@@ -3,11 +3,15 @@ import {
   CreatedShotLink,
   GetOne,
   Getall,
+  DeleteAll,
+  VerifyUrl,
 } from "../controllers/url.controller.js";
 
 const router = Router();
 router.get("/", Getall);
 router.get("/:id", GetOne);
 router.post("/", CreatedShotLink);
+router.delete("/", DeleteAll);
+router.post("/verify", VerifyUrl);
 
 export default router;
